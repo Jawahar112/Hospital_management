dd3663ddbd600355e176847730aec64271789e63
jawahar112 <bharathijawahar583@gmail.com>
2024-01-06 20:51:41 +0530

    app.js updatte

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,10 +1,17 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import Register from './components/patient_register/register';
 export default function App() {
 
   return (
-    <h1>hello from react</h1> 
-    
+ 
+  <BrowserRouter>
+  <Routes>
+    <Route path='/register' exact element={<Register/>}/>
+  </Routes>
+  </BrowserRouter>
     
     )
   }
