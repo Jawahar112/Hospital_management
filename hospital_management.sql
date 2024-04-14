3e901849575942043eb77f15579bec11c2c18981
jawahar112 <bharathijawahar583@gmail.com>
2024-04-14 20:51:06 +0530

    database file

--- hospital_management.sql
+++ hospital_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 31, 2024 at 08:11 AM
+-- Generation Time: Apr 14, 2024 at 05:17 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -49,9 +49,42 @@ INSERT INTO `admin` (`email`, `password`, `phoneno`) VALUES
 CREATE TABLE `appointment` (
   `appoinment_id` int(11) NOT NULL,
   `patient_id` varchar(30) NOT NULL,
-  `Doctor_id` char(5) NOT NULL,
-  `Date` date NOT NULL
+  `Date` date NOT NULL,
+  `appoinment_time` time NOT NULL,
+  `Doctor_id` varchar(30) NOT NULL,
+  `fees` varchar(30) NOT NULL,
+  `Status` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `patient_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`appoinment_id`, `patient_id`, `Date`, `appoinment_time`, `Doctor_id`, `fees`, `Status`, `email`, `patient_name`) VALUES
+(31, '1be96536-bbb3-4f05-9ac9-434596', '2024-04-15', '10:00:00', '5833a2fc-e8d8-457e-8dce-b60f4c', 'pending', 'Booked', 'bh@gmail.com', 'jawahar'),
+(32, '8a8965ca-0897-4743-acab-45db08', '2024-04-15', '10:30:00', '5833a2fc-e8d8-457e-8dce-b60f4c', 'pending', 'Booked', 'fd@gmail.xom', 'fd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `available_time`
+--
+
+CREATE TABLE `available_time` (
+  `Available_id` varchar(30) NOT NULL,
+  `Doctor_id` varchar(30) NOT NULL,
+  `Avilable_time` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`Avilable_time`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `available_time`
+--
+
+INSERT INTO `available_time` (`Available_id`, `Doctor_id`, `Avilable_time`) VALUES
+('1', '5833a2fc-e8d8-457e-8dce-b60f4c', '{\"Monday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Tuesday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Wednesday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Thursday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Friday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Saturnday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Sunday\": [\"12:00\", \"1:00\"]}'),
+('2', 'b86e6760-cc59-4ee7-a6dd-09e397', '{\"Monday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Tuesday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Wednesday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Thursday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Friday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Saturnday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Sunday\": [\"12:00\", \"1:00\"]}');
 
 -- --------------------------------------------------------
 
@@ -64,6 +97,19 @@ CREATE TABLE `department` (
   `Department_name` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`Department_id`, `Department_name`) VALUES
+('0', 'Anesthetics'),
+('1', 'cardiology'),
+('2', 'ENT'),
+('3', 'neurology'),
+('4', 'urology'),
+('5', 'Renal unit'),
+('6', 'Oncology');
+
 -- --------------------------------------------------------
 
 --
@@ -71,22 +117,25 @@ CREATE TABLE `department` (
 --
 
 CREATE TABLE `doctors` (
-  `Doctor_id` char(5) NOT NULL,
+  `Id` varchar(30) NOT NULL,
   `Doctor_name` varchar(20) NOT NULL,
-  `Department_id` char(5) NOT NULL,
   `phone_no` int(15) NOT NULL,
   `specialist` varchar(30) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `password` varchar(11) NOT NULL
+  `password` varchar(11) NOT NULL,
+  `department_id` varchar(30) NOT NULL,
+  `salary` int(10) NOT NULL,
+  `age` int(4) NOT NULL,
+  `degree` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `doctors`
 --
 
-INSERT INTO `doctors` (`Doctor_id`, `Doctor_name`, `Department_id`, `phone_no`, `specialist`, `email`, `password`) VALUES
-('0', 'jansi', '0', 65656, 'cardiology', '0', '0'),
-('1', 'jawahar', '1', 54545, 'arthologist', 'doctor@gmail.com', '1234');
+INSERT INTO `doctors` (`Id`, `Doctor_name`, `phone_no`, `specialist`, `email`, `password`, `department_id`, `salary`, `age`, `degree`) VALUES
+('5833a2fc-e8d8-457e-8dce-b60f4c', 'jansirani', 19289182, 'psyhologist', 'dkh@gmail.com', '1234', '2', 0, 0, 'Mbbs,bds'),
+('b86e6760-cc59-4ee7-a6dd-09e397', 'natraj perumal', 832983928, 'ent specialist', 'natrajperumal12@gmail.com', '1234', '1', 0, 0, 'Mbbs,Bds');
 
 -- --------------------------------------------------------
 
@@ -166,7 +215,9 @@ CREATE TABLE `patients` (
 
 INSERT INTO `patients` (`patient_id`, `first_name`, `last_name`, `Nationality`, `Address`, `DoA`, `Email`, `Phone_no`, `Emergency_contact`, `gender`, `Bloodtype`, `Age`, `weight`) VALUES
 ('1be96536-bbb3-4f05-9ac9-434596', 'jawahar', 'B', 'Indian', 'rer34', '0000-00-00', 'bh@gmail.com', 4545, '4545', 'male', '', 0, ''),
-('8a8965ca-0897-4743-acab-45db08', 'fd', 'df', 'Indian', 'df', '0000-00-00', 'fd@gmail.xom', 545, '45', 'male', 'A+', 0, '');
+('8a8965ca-0897-4743-acab-45db08', 'fd', 'df', 'Indian', 'df', '0000-00-00', 'fd@gmail.xom', 545, '45', 'male', 'A+', 0, ''),
+('a0bc3a1e-32fb-43f1-8e57-2bc904', 'ENT', 'nataraj', 'natraj@gmail.co', '1234', '0000-00-00', 'undefined', 0, 'undefined', 'undefined', '8392893', 0, ''),
+('c87ec922-e166-45f1-bda8-e6d95f', 'hg', 'g', 'Indian', 'rer', '0000-00-00', 'er@gmail.com', 65656, '3434', 'male', '', 0, '');
 
 -- --------------------------------------------------------
 
@@ -208,6 +259,19 @@ CREATE TABLE `room` (
   `patient_id` char(5) NOT NULL,
   `staff_id` char(5) NOT NULL,
   `Admisson_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `Doctor_Name` varchar(30) NOT NULL,
+  `Doctor_id` varchar(30) NOT NULL,
+  `Start_time` time NOT NULL,
+  `End_time` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -255,7 +319,15 @@ CREATE TABLE `ward` (
 --
 ALTER TABLE `appointment`
   ADD PRIMARY KEY (`appoinment_id`),
-  ADD KEY `pkey` (`patient_id`);
+  ADD KEY `doctor_key` (`Doctor_id`),
+  ADD KEY `patient_key` (`patient_id`);
+
+--
+-- Indexes for table `available_time`
+--
+ALTER TABLE `available_time`
+  ADD PRIMARY KEY (`Available_id`),
+  ADD KEY `did` (`Doctor_id`);
 
 --
 -- Indexes for table `department`
@@ -267,7 +339,8 @@ ALTER TABLE `department`
 -- Indexes for table `doctors`
 --
 ALTER TABLE `doctors`
-  ADD PRIMARY KEY (`Doctor_id`);
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `dep_id` (`department_id`);
 
 --
 -- Indexes for table `medicalrecords`
@@ -325,6 +398,16 @@ ALTER TABLE `ward`
   ADD PRIMARY KEY (`Ward_id`);
 
 --
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `appoinment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -332,8 +415,22 @@ ALTER TABLE `ward`
 -- Constraints for table `appointment`
 --
 ALTER TABLE `appointment`
-  ADD CONSTRAINT `d_key` FOREIGN KEY (`Doctor_id`) REFERENCES `doctors` (`Doctor_id`),
+  ADD CONSTRAINT `doctor_key` FOREIGN KEY (`Doctor_id`) REFERENCES `doctors` (`Id`),
+  ADD CONSTRAINT `patient_key` FOREIGN KEY (`patient_id`) REFERENCES `patients` (`patient_id`),
   ADD CONSTRAINT `pkey` FOREIGN KEY (`patient_id`) REFERENCES `patients` (`patient_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `available_time`
+--
+ALTER TABLE `available_time`
+  ADD CONSTRAINT `did` FOREIGN KEY (`Doctor_id`) REFERENCES `doctors` (`Id`);
+
+--
+-- Constraints for table `doctors`
+--
+ALTER TABLE `doctors`
+  ADD CONSTRAINT `d_id` FOREIGN KEY (`Department_id`) REFERENCES `department` (`Department_id`),
+  ADD CONSTRAINT `dep_id` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`);
 
 --
 -- Constraints for table `medicalrecords`
