84b727f788497aab478ae1d38a91cd0833149e5f
jawahar112 <bharathijawahar583@gmail.com>
2024-04-15 22:06:30 +0530

    mess

--- hospital_management(1).sql
+++ hospital_management(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2024 at 05:17 PM
+-- Generation Time: Apr 15, 2024 at 06:34 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -63,8 +63,13 @@ CREATE TABLE `appointment` (
 --
 
 INSERT INTO `appointment` (`appoinment_id`, `patient_id`, `Date`, `appoinment_time`, `Doctor_id`, `fees`, `Status`, `email`, `patient_name`) VALUES
-(31, '1be96536-bbb3-4f05-9ac9-434596', '2024-04-15', '10:00:00', '5833a2fc-e8d8-457e-8dce-b60f4c', 'pending', 'Booked', 'bh@gmail.com', 'jawahar'),
-(32, '8a8965ca-0897-4743-acab-45db08', '2024-04-15', '10:30:00', '5833a2fc-e8d8-457e-8dce-b60f4c', 'pending', 'Booked', 'fd@gmail.xom', 'fd');
+(32, '8a8965ca-0897-4743-acab-45db08', '2024-04-15', '10:30:00', '5833a2fc-e8d8-457e-8dce-b60f4c', 'pending', 'Booked', 'fd@gmail.xom', 'fd'),
+(35, 'a0bc3a1e-32fb-43f1-8e57-2bc904', '2024-04-15', '12:00:00', '5833a2fc-e8d8-457e-8dce-b60f4c', 'pending', 'Booked', 'bh@gmail.com', 'jawahar'),
+(36, 'a0bc3a1e-32fb-43f1-8e57-2bc904', '2024-04-15', '12:00:00', '5833a2fc-e8d8-457e-8dce-b60f4c', 'pending', 'Booked', 'bh@gmail.com', 'jawahar'),
+(37, '3968ed1e-4815-451a-a171-e90756', '2024-04-15', '01:00:00', '5833a2fc-e8d8-457e-8dce-b60f4c', 'pending', 'Booked', 'bh@gmail.com', 'jawahar '),
+(38, 'a0bc3a1e-32fb-43f1-8e57-2bc904', '2024-04-15', '01:00:00', '5833a2fc-e8d8-457e-8dce-b60f4c', 'pending', 'Booked', 'bh@gmail.com', 'jawahar'),
+(40, '3968ed1e-4815-451a-a171-e90756', '2024-04-16', '10:00:00', '5833a2fc-e8d8-457e-8dce-b60f4c', 'pending', 'Booked', 'bh@gmail.com', 'jawahar '),
+(41, 'a0bc3a1e-32fb-43f1-8e57-2bc904', '2024-04-16', '10:00:00', '5833a2fc-e8d8-457e-8dce-b60f4c', 'pending', 'Booked', 'bh@gmail.com', 'jawahar');
 
 -- --------------------------------------------------------
 
@@ -83,8 +88,7 @@ CREATE TABLE `available_time` (
 --
 
 INSERT INTO `available_time` (`Available_id`, `Doctor_id`, `Avilable_time`) VALUES
-('1', '5833a2fc-e8d8-457e-8dce-b60f4c', '{\"Monday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Tuesday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Wednesday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Thursday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Friday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Saturnday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Sunday\": [\"12:00\", \"1:00\"]}'),
-('2', 'b86e6760-cc59-4ee7-a6dd-09e397', '{\"Monday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Tuesday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Wednesday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Thursday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Friday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Saturnday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Sunday\": [\"12:00\", \"1:00\"]}');
+('1', '5833a2fc-e8d8-457e-8dce-b60f4c', '{\"Monday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Tuesday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Wednesday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Thursday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Friday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Saturnday\": [\"10:00\", \"10:30\", \"12:00\", \"1:00\", \"1:30\", \"2:00\"], \"Sunday\": [\"12:00\", \"1:00\"]}');
 
 -- --------------------------------------------------------
 
@@ -135,29 +139,7 @@ CREATE TABLE `doctors` (
 
 INSERT INTO `doctors` (`Id`, `Doctor_name`, `phone_no`, `specialist`, `email`, `password`, `department_id`, `salary`, `age`, `degree`) VALUES
 ('5833a2fc-e8d8-457e-8dce-b60f4c', 'jansirani', 19289182, 'psyhologist', 'dkh@gmail.com', '1234', '2', 0, 0, 'Mbbs,bds'),
-('b86e6760-cc59-4ee7-a6dd-09e397', 'natraj perumal', 832983928, 'ent specialist', 'natrajperumal12@gmail.com', '1234', '1', 0, 0, 'Mbbs,Bds');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `medicalrecords`
---
-
-CREATE TABLE `medicalrecords` (
-  `patient_id` int(11) NOT NULL,
-  `treatment` varchar(11) NOT NULL,
-  `testresults` varchar(11) NOT NULL,
-  `appoinment_id` int(11) DEFAULT NULL,
-  `record_Id` int(11) NOT NULL,
-  `bills` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `medicalrecords`
---
-
-INSERT INTO `medicalrecords` (`patient_id`, `treatment`, `testresults`, `appoinment_id`, `record_Id`, `bills`) VALUES
-(5, 'yt', 'ty', NULL, 2, 'ty');
+('c037c690-886a-4081-9ff8-dd9bfa', 'mano', 832983823, 'orthologist', 'doctor@gmail.com', '1234', '3', 0, 0, '');
 
 -- --------------------------------------------------------
 
@@ -214,10 +196,9 @@ CREATE TABLE `patients` (
 --
 
 INSERT INTO `patients` (`patient_id`, `first_name`, `last_name`, `Nationality`, `Address`, `DoA`, `Email`, `Phone_no`, `Emergency_contact`, `gender`, `Bloodtype`, `Age`, `weight`) VALUES
-('1be96536-bbb3-4f05-9ac9-434596', 'jawahar', 'B', 'Indian', 'rer34', '0000-00-00', 'bh@gmail.com', 4545, '4545', 'male', '', 0, ''),
-('8a8965ca-0897-4743-acab-45db08', 'fd', 'df', 'Indian', 'df', '0000-00-00', 'fd@gmail.xom', 545, '45', 'male', 'A+', 0, ''),
-('a0bc3a1e-32fb-43f1-8e57-2bc904', 'ENT', 'nataraj', 'natraj@gmail.co', '1234', '0000-00-00', 'undefined', 0, 'undefined', 'undefined', '8392893', 0, ''),
-('c87ec922-e166-45f1-bda8-e6d95f', 'hg', 'g', 'Indian', 'rer', '0000-00-00', 'er@gmail.com', 65656, '3434', 'male', '', 0, '');
+('3968ed1e-4815-451a-a171-e90756', 'jawahar ', 'B', 'Indian', 're', '0000-00-00', 'bh@gmail.com', 54545, '45454545', 'male', 'A+', 0, ''),
+('8a8965ca-0897-4743-acab-45db08', 'fd', 'df', 'Indian', 'df', '2024-04-09', 'fd@gmail.xom', 545, '45', 'male', 'A+', 0, ''),
+('a0bc3a1e-32fb-43f1-8e57-2bc904', 'jawahar', 'B', 'Indian', '1234', '2024-04-09', 'bh@gmail.com', 343434545, '545465656', 'undefined', 'A-', 0, '');
 
 -- --------------------------------------------------------
 
@@ -343,13 +324,6 @@ ALTER TABLE `doctors`
   ADD KEY `dep_id` (`department_id`);
 
 --
--- Indexes for table `medicalrecords`
---
-ALTER TABLE `medicalrecords`
-  ADD PRIMARY KEY (`record_Id`),
-  ADD KEY `ap_key` (`appoinment_id`);
-
---
 -- Indexes for table `nurse`
 --
 ALTER TABLE `nurse`
@@ -405,7 +379,7 @@ ALTER TABLE `ward`
 -- AUTO_INCREMENT for table `appointment`
 --
 ALTER TABLE `appointment`
-  MODIFY `appoinment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `appoinment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- Constraints for dumped tables
@@ -416,14 +390,14 @@ ALTER TABLE `appointment`
 --
 ALTER TABLE `appointment`
   ADD CONSTRAINT `doctor_key` FOREIGN KEY (`Doctor_id`) REFERENCES `doctors` (`Id`),
-  ADD CONSTRAINT `patient_key` FOREIGN KEY (`patient_id`) REFERENCES `patients` (`patient_id`),
+  ADD CONSTRAINT `patient_key` FOREIGN KEY (`patient_id`) REFERENCES `patients` (`patient_id`) ON DELETE CASCADE,
   ADD CONSTRAINT `pkey` FOREIGN KEY (`patient_id`) REFERENCES `patients` (`patient_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `available_time`
 --
 ALTER TABLE `available_time`
-  ADD CONSTRAINT `did` FOREIGN KEY (`Doctor_id`) REFERENCES `doctors` (`Id`);
+  ADD CONSTRAINT `did` FOREIGN KEY (`Doctor_id`) REFERENCES `doctors` (`Id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `doctors`
@@ -431,13 +405,6 @@ ALTER TABLE `available_time`
 ALTER TABLE `doctors`
   ADD CONSTRAINT `d_id` FOREIGN KEY (`Department_id`) REFERENCES `department` (`Department_id`),
   ADD CONSTRAINT `dep_id` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`);
-
---
--- Constraints for table `medicalrecords`
---
-ALTER TABLE `medicalrecords`
-  ADD CONSTRAINT `ap_key` FOREIGN KEY (`appoinment_id`) REFERENCES `appointment` (`appoinment_id`),
-  ADD CONSTRAINT `pk` FOREIGN KEY (`patient_id`) REFERENCES `patients` (`patient_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
